13c6df80b296772f26b361098e9dba4ff7b90e8a
ilutsav <ilutsav@gmail.com>
2023-07-14 12:42:11 +0545

    setting the timer and making it stop at 0

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,13 +10,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int initialValue = 10;
+  Timer? timer;
+  @override
+  void initState() {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        initialValue--;
+        if (initialValue == 0) {
+          timer.cancel();
+        }
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [Image.asset('assets/stopwatch.png')],
+          children: [
+            Image.asset('assets/stopwatch.png'),
+            const SizedBox(
+              height: 32,
+            ),
+            Text(
+              initialValue.toString(),
+              style: const TextStyle(fontSize: 72),
+            )
+          ],
         ),
       ),
     );
